85cfccfe88190fcebd8773ab11f130d6756f8e6a
YoungCat CHEN <chxuexuan@gmail.com>
2013-09-13 17:01:27 +0800

    Fixed a bug.

--- profile.d/6-locale.sh
+++ profile.d/6-locale.sh
@@ -1,11 +1,11 @@
-if [ -n "$LANG2" ]; then
+if [ -n "$lang" ]; then
   export LANG="$lang"
   export LC_ALL="$lang"
   _echo2cache "export LANG='$lang'"
   _echo2cache "export LC_ALL='$lang'"
 fi
 
-if [ -n "$LANGUAGE2" ]; then
+if [ -n "$language" ]; then
   export LANGUAGE="$language"
   _echo2cache "export LANGUAGE='$language'"
 fi
