5915fc96b9035dfaedc49e7d0152a236b6a26bfd
YoungCatChen <chxuexuan@gmail.com>
2014-12-01 15:41:02 +0100

    Reset HISTSIZE from 1000 to 100000.

--- bashrc.d/00-config.bash
+++ bashrc.d/00-config.bash
@@ -5,8 +5,8 @@ shopt -s cdspell checkhash checkwinsize cmdhist expand_aliases extglob \
 	histappend interactive_comments no_empty_cmd_completion progcomp
 
 HISTCONTROL=ignoredups:ignorespace
-HISTSIZE=1000
-HISTFILESIZE=2000
+HISTSIZE=100000
+HISTFILESIZE=200000
 HISTTIMEFORMAT='%Y-%m-%d %H:%M:%S  '
 
 #### direct2cache head END ####
