2f526197b34e80b0b6aace951d381226d448fbc8
YoungCat CHEN <chxuexuan@gmail.com>
2016-07-07 19:26:18 -0700

    Minor tweaking of zpreztorc.

--- zpreztorc.zsh
+++ zpreztorc.zsh
@@ -33,6 +33,7 @@ zstyle ':prezto:load' pmodule \
   'gnu-utility' \
   'utility' \
   'completion' \
+  'git' \
   'command-not-found' \
   'syntax-highlighting' \
   'autosuggestions' \
@@ -96,7 +97,7 @@ zstyle ':prezto:module:history-substring-search' globbing-flags ''
 # Set the prompt theme to load.
 # Setting it to 'random' loads a random theme.
 # Auto set to 'off' on dumb terminals.
-zstyle ':prezto:module:prompt' theme 'agnoster'
+zstyle ':prezto:module:prompt' theme 'sorin'
 
 #
 # Ruby
