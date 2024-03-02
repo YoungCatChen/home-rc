49396e6999129a1b2e10a08a35000acf4e428956
YoungCatChen <YoungCatChen@users.noreply.github.com>
2024-03-01 16:00:03 -0800

    Slight changes to update-home-rc.sh.

--- misc-tools/update-home-rc.sh
+++ misc-tools/update-home-rc.sh
@@ -3,6 +3,10 @@
 # Updates home-rc by downloading it from Github, unzipping it and replacing the
 # old home-rc with the newly downloaded version.
 #
+# In most cases `git clone` or `git pull` would be a better option to update
+# home-rc scripts. This script is helpful only when `git` is not available,
+# e.g. in an embedded system where storage is limited.
+#
 # Note that this script will use the current home-rc's parent directory as
 # working directory for downloading and unzipping.
 
@@ -13,7 +17,7 @@ cd "$( dirname "$( realpath "$0" )" )"/../..
 pwd
 rm -rf home-rc-master master.zip
 wget https://github.com/YoungCatChen/home-rc/archive/refs/heads/master.zip
-unzip master.zip
+unzip -q master.zip
 test -r home-rc-master/install.sh
 rm -rf home-rc
 mv home-rc-master home-rc
