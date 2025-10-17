01dc4d5a0ef1369fa38be205aa58e810f79f050b
YoungCatChen <YoungCatChen@users.noreply.github.com>
2025-10-16 23:20:56 -0700

    Added pstreethis that shows only current process and its ancestors.

--- fish/functions/pstree.fish
+++ fish/functions/pstree.fish
@@ -9,10 +9,12 @@ set -g helpdoc (pstree --help 2>&1)
 
 set -l hide       # hide threads, show only processes
 set -l highlight  # highlight current process and its ancestors
+set -l thisproc   # show only current process and its ancestors
 set -l uid        # show uid transitions
 
 helpdoc_contains --hide-threads  && set hide --hide-threads
 helpdoc_contains --highlight-all && set highlight --highlight-all
+helpdoc_contains '-p pid'        && set thisproc -p '$fish_pid'
 helpdoc_contains --uid-changes   && set uid --uid-changes
 
 set -l unicode  # use graphics chars for tree drawing characters
@@ -34,3 +36,4 @@ set --erase helpdoc
 
 alias pstree="pstree $hide $highlight $uid $unicode"
 alias pstreeme="pstree $user"
+alias pstreethis="pstree $thisproc"
--- profile.d/pstree.sh
+++ profile.d/pstree.sh
@@ -2,7 +2,7 @@
 # To keep functionality in sync with `pstree.fish`.
 
 __setup_pstree() {
-  unset -f pstree pstreeme __setup_pstree
+  unset -f pstree pstreeme pstreethis __setup_pstree
 
   helpdoc_contains() {
     string_contains "$helpdoc" " $1 "
@@ -14,6 +14,8 @@ __setup_pstree() {
   helpdoc_contains --hide-threads  && hide=--hide-threads
   # highlight current process and its ancestors
   helpdoc_contains --highlight-all && highlight=--highlight-all
+  # show only current process and its ancestors
+  helpdoc_contains '-p pid'        && thisproc='-p $$'
   # show uid transitions
   helpdoc_contains --uid-changes   && uid=--uid-changes
 
@@ -34,16 +36,23 @@ __setup_pstree() {
   unset -f helpdoc_contains
   alias pstree="pstree $hide $highlight $uid $unicode"
   alias pstreeme="pstree $user"
+  alias pstreethis="pstree $thisproc"
 }
 
 pstree() {
-  local helpdoc hide highlight uid unicode user
+  local helpdoc hide highlight thisproc uid unicode user
   __setup_pstree
   pstree $hide $highlight $uid $unicode
 }
 
 pstreeme() {
-  local helpdoc hide highlight uid unicode user
+  local helpdoc hide highlight thisproc uid unicode user
   __setup_pstree
   pstree $hide $highlight $uid $unicode $user
+}
+
+pstreethis() {
+  local helpdoc hide highlight thisproc uid unicode user
+  __setup_pstree
+  eval pstree $hide $highlight $uid $unicode $thisproc
 }
